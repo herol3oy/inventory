497d0b1696034b99fa6262c1c0e71af53a9c471f
Demah <hamedsedighi@gmail.com>
2023-09-21 18:05:47 +0200

    add chevron arrow

--- react-native/src/components/ProductItem.tsx
+++ react-native/src/components/ProductItem.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { View, StyleSheet } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { ProductThumbnail } from "./ProductThumbnail";
 import { ProductName } from "./ProductName";
 import { ProductDate } from "./ProductDate";
 import { calculateProductIsNew } from "../utils/calculate-product-is-new";
 import { ProductBadge } from "./ProductBadge";
+import { ChevronArrowImage } from "./ChevronArrowImage";
 
 interface ProductItemProps {
   productName: string;
@@ -18,7 +21,13 @@ export const ProductItem = ({
   productPosted,
   productCategories
 }: ProductItemProps) => {
+  const [isCategoryOpen, setIsCategoryOpen] = useState<boolean>(true);
+
   const isProductNew: boolean = calculateProductIsNew(productPosted);
+
+  const handleCategoryToggle = () => {
+    setIsCategoryOpen((open) => !open);
+  };
 
   return (
     <View style={styles.productContainer}>
@@ -31,6 +40,9 @@ export const ProductItem = ({
           </View>
           <View style={styles.badgeContainer}>
             {isProductNew && <ProductBadge name="New" />}
+            <TouchableWithoutFeedback onPress={handleCategoryToggle}>
+              <ChevronArrowImage isCategoryOpen={isCategoryOpen} />
+            </TouchableWithoutFeedback>
           </View>
         </View>
       </View>
