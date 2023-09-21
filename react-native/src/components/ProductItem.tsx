ddcf8e6285320b50521ad22bd41e0acda08624ad
Demah <hamedsedighi@gmail.com>
2023-09-21 17:28:04 +0200

    add product name

--- react-native/src/components/ProductItem.tsx
+++ react-native/src/components/ProductItem.tsx
@@ -1,4 +1,6 @@
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { ProductThumbnail } from "./ProductThumbnail";
+import { ProductName } from "./ProductName";
 
 interface ProductItemProps {
   productName: string;
@@ -7,6 +9,43 @@ interface ProductItemProps {
   productCategories: string;
 }
 
-export const ProductItem = (props: ProductItemProps) => {
-  return <Text>{JSON.stringify(props, null, 2)}</Text>;
+export const ProductItem = ({
+  productName,
+  productImage,
+  productPosted,
+  productCategories
+}: ProductItemProps) => {
+  return (
+    <View style={styles.productContainer}>
+      <ProductThumbnail productImage={productImage} />
+      <View style={styles.productDetailsContainer}>
+        <View style={styles.productDetailsHeader}>
+          <View>
+            <ProductName name={productName} />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  productContainer: {
+    flexDirection: "row",
+    backgroundColor: "#F8F9FC",
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    columnGap: 12,
+    padding: 8,
+    elevation: 2
+  },
+  productDetailsContainer: {
+    rowGap: 12,
+    flex: 1
+  },
+  productDetailsHeader: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between"
+  }
+});
